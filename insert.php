2138ec4baf8ad3e7741857c99bc0686a4afad052
deekshithjohnbaby <37650809+deekshithjohnbaby@users.noreply.github.com>
2018-06-06 15:22:21 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,16 +1,31 @@
 <?php
-$servername = "testdb";
-$username = "root";
-$password = "amveryhappy";
-$db_name = "testdb";
-// Create connection
-$conn = new mysqli($servername, $username, $password,$db_name);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-//echo "Connected successfully";
-// links
-$home_url = "index.php";
-$insert_url = "insert.php";
+include_once("configuration.php");
+if(isset($_POST['fname']) && isset($_POST['sname']) && isset($_POST['email']) && isset($_POST['mobile']))
+{
+	  $fname= $_POST['fname'];
+	  $sname= $_POST['sname'];
+	  $email= $_POST['email'];
+	  $mob= $_POST['mobile'];
+	 
+	  
+	  $sql = "INSERT INTO tbl_registration (first_name, last_name, email_id,phone)
+VALUES ('".$fname."', '".$sname."', '".$email."','".$mob."')";
+
+
+if ($conn->query($sql)) {
+	$a['value'] = 1;
+    echo json_encode($a);
+} else {
+	$a['value'] = 0;
+   echo json_encode($a);
+}
+	  
+}
+else
+{
+	 $a['value'] = 3;
+	 echo json_encode($a);
+}
+
+
 ?>
